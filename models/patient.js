f36e5402f06ada14b57ffdb60e26ff752ec1eba0
Osyrus85 <osyrus85@gmail.com>
2020-08-31 10:40:22 -0500

    Capital P for patient

--- models/patient.js
+++ models/patient.js
@@ -1,15 +1,15 @@
 // Creates a "Patient" model that matches up with DB
 module.exports = function (sequelize, DataTypes) {
-  var Patient = sequelize.define("patient", {
+  var Patient = sequelize.define("Patient", {
     first: {
       type: DataTypes.STRING,
       allowNull: false,
-      isAlpha: true,
+     
     },
     last: {
       type: DataTypes.STRING,
       allowNull: false,
-      isAlpha: true,
+     
     },
     email: {
       type: DataTypes.STRING,
